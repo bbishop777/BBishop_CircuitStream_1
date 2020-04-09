4a7575fa1351f2d562464ef06be655caf4fab1d1
BBishop <brad.bishop68@gmail.com>
2020-04-09 13:20:55 -1000

    New Scene included and AR scene updated
    New Commit including RollerBall game and AR update

---
+++ Mar-AN-2/Assets/Scripts/MiniGameRollerBall Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        
+    }
+
+    // Update is called once per frame but LateUpdate is called after everything else is completed in the frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        
+    }
+}
