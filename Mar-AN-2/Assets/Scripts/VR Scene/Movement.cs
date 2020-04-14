37a46e78fdc17e5b5743d51713e0764566cf81e4
BBishop <brad.bishop68@gmail.com>
2020-04-14 02:12:05 -1000

    Retry Last Commit
    Last Commit did not show on Master online.  Reattempting.

---
+++ Mar-AN-2/Assets/Scripts/VR Scene/Movement.cs
@@ -0,0 +1,19 @@
+﻿//The first 3 lines below are libraries.  Libraries bring in methods and operations to use within C# because it is not
+//robust on its own.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  //This library brings in stuff like transform, etc
+
+public class Movement : MonoBehaviour   //public means it is accessible outside this file scripts...other scripts can reach
+                                        //  it as well as Unity Inspector. "private" restricts access to this script only. 
+                                        //"Movement" must be used here because it is the name of the script.
+{
+    public float m_speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
+        
+    }
+}
